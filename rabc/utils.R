32731c0cdb0e7d550fb3b8bbba773f66feed441f
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-12-14 00:28:24 +0300

    added utils.R

---
+++ rabc/utils.R
@@ -0,0 +1,13 @@
+model <- function(x){
+  
+}
+
+borders <- c(2., 5., 10., 15., 20.)
+
+det_fractions_in_bsl_ranges <- function(bsls_sthr_status, borders){
+  for (i in seq(1, length(borders)-1)) {
+    statuses = subset(dat, baseline_ed>borders[i] & baseline_ed<borders[i+1], status)
+    fractions[i] <- sum(statuses)/nrow(statuses)
+  }
+  return(fractions)
+}
