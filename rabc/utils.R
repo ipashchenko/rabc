b55f243c8935eee55e9276b54c0ed004750a4b5f
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-12-15 00:30:57 +0400

    test commit from windows rstudio

--- rabc/utils.R
+++ rabc/utils.R
@@ -8,7 +8,7 @@ model <- function(x){
 }
 
 borders <- c(2., 5., 10., 15., 20.)
-
+#test commit windows
 det_fractions_in_bsl_ranges <- function(bsls_sthr_status, borders){
   for (i in seq(1, length(borders)-1)) {
     statuses = subset(dat, baseline_ed>borders[i] & baseline_ed<borders[i+1], status)
