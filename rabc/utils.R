12d063a64465351a4ed490c95d82efa00df56153
Ilya Pashchenko <in4pashchenko@gmail.com>
2014-12-29 11:42:23 +0300

    Added flux of circular gaussian source

--- rabc/utils.R
+++ rabc/utils.R
@@ -1,5 +1,6 @@
 library(gdata)
 col.names <- c('source', 'bl', 's_thr', 'status')
+setwd("/home/ilya/code/rabc/rabc")
 data = read.table('data_to_R.txt', col.names=col.names)
 
 # Model that given parameters vector ``x`` returns vector of summary statistic
@@ -15,11 +16,37 @@ det_fractions_in_bsl_ranges <- function(data, borders){
     statuses = subset(data, bl>borders[i] & bl<borders[i+1], status)
     y = subset(statuses, statuses == 'y')
     print(y)
-    fractions[i] <- as.double(length(y))/length(statuses)
+    fractions[i] <- as.double(length(y$status))/length(statuses$status)
   }
   return(fractions)
 }
 
+#def flux_(b, v0, tb):
+#  """
+#    Flux of circular gaussian source with full flux ``v0`` and brightness
+#    temperature ``tb`` at baseline ``b``.
+#    :param b:
+#        Baseline [baseline, ED]
+#    :param v0:
+#        Amplitude of component [Jy].
+#    :param tb:
+#        Brightness temperature of source [K].
+#    :return:
+#        Value of correlated flux.
+#    """
+#    b = b * 12742. * 10. ** 3
+#    k = 1.38 * 10 ** (-23)
+#    return v0 * np.exp(-math.pi * b ** 2. * v0 * 10 ** (-26) / (2. * k * tb))
+
+flux<-function(x){
+  x[1] = x[1] * 12742. * 10. ** 3
+  k = 1.38 * 10 ** (-23)
+  pi = 3.14159
+  result = x[2] * exp(-pi * x[1] ** 2. * x[2] * 10 ** (-26) / (2. * k * x[3]))
+  return(result)
+}
+
+# function must accept vector of model parameters and return summary statistics
 toy_model<-function(x){
   
 }
