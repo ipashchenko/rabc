801356e485a96875f92ec9fc4a8cb2b253cb8400
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-02-03 22:03:04 +0300

    Added flux of ell.gaussian

--- rabc/utils.R
+++ rabc/utils.R
@@ -3,13 +3,8 @@ col.names <- c('source', 'bl', 's_thr', 'status')
 setwd("/home/ilya/code/rabc/rabc")
 data = read.table('data_to_R.txt', col.names=col.names)
 
-# Model that given parameters vector ``x`` returns vector of summary statistic
-model <- function(x){
-  
-}
+borders <- c(2., 5., 10., 15., 20., 30.)
 
-borders <- c(2., 5., 10., 15., 20.)
-#test commit windows
 det_fractions_in_bsl_ranges <- function(data, borders){
   fractions <- vector(length=length(borders)-1)
   for (i in seq(1, length(borders)-1)) {
@@ -24,17 +19,36 @@ det_fractions_in_bsl_ranges <- function(data, borders){
 #def flux_(b, v0, tb):
 # Function that reads matrix of (bl, v0, tb) values and returns vectors of fluxes
 flux<-function(x){
+  # Transform baselines from ED to m
   x[ , 1] = x[ , 1] * 12742. * 10. ** 3
+  # Transform fluxes from Jy to W / (m**2 * Hz)
+  x[ , 2] = x[ , 2] * 10 ** (-26)
   k = 1.38 * 10 ** (-23)
   pi = 3.14159
-  result = x[ , 2] * exp(-pi * x[ , 1] ** 2. * x[ , 2] * 10 ** (-26) / (2. * k * x[ , 3]))
+  result = x[ , 2] * exp(-pi * x[ , 1] ** 2. * x[ , 2] / (2. * k * x[ , 3]))
+  # Back to Jy
+  result = result * 10 ** 26
   return(result)
 }
 
-# example
-x<-c(-0.43, 0.94, 28.8, 2.1)
+#def flux(b, v0, tb, e, dfi)
+flux_ell<-function(x){
+  # Transform baselines from ED to m
+  x[ , 1] = x[ , 1] * 12742. * 10. ** 3
+  # Transform fluxes from Jy to W / (m**2 * Hz)
+  x[ , 2] = x[ , 2] * 10 ** (-26)
+  k = 1.38 * 10 ** (-23)
+  pi = 3.14159
+  result = x[ , 2] * exp(-pi * x[ , 1] ** 2. * x[ , 2] * (x[ , 4] * cos(x[ , 5]) ** 2. + x[ , 4] ** (-1) * sin(x[ , 5]) ** 2.) / (2. * k * x[ , 3]))
+  # Back to Jy
+  result = result * 10 ** 26
+  return(result)
+}
+
 # function must accept vector of model parameters and return summary statistics
 # x - vector of parameters of population distribution (mu_logv0, std_logv0, mu_logtb, std_logtb)
+# example
+# x<-c(-0.43, 0.94, 28.8, 2.1)
 model1<-function(x){
   fractions <- vector(length=length(borders)-1)
   for (i in seq(1, length(borders)-1)) {
@@ -63,9 +77,9 @@ model1<-function(x){
 library("EasyABC")
 library("abc")
 # Define priors
-prior1=list(c("unif", -3, 1), c("unif", 0, 5), c("unif", 26, 32), c("unif", 0, 7.5))
+prior1=list(c("normal", -0.94, 0.3), c("unif", 0., 5.), c("unif", 26, 32), c("unif", 0, 7.5))
 # Data summary statistics
-sum_stat_obs=c(0.68085106, 0.43043478, 0.20297030, 0.09770115)
+sum_stat_obs=c(0.68085106, 0.43043478, 0.20297030, 0.09770115, 0.02116402)
 set.seed(1)
 
 # Doing rejection sampling
