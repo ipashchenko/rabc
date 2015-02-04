8054aa4c5e88bb01b85380cf8e9828baf689d0a2
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-02-04 19:11:41 +0300

    fix prior (VSOP). Coding simulation of survey

--- rabc/utils.R
+++ rabc/utils.R
@@ -100,6 +100,32 @@ model2<-function(x){
   return(fractions)
 }
 
+# as model2 but for multicore
+model2m<-function(x){
+  set.seed(x[1])
+  fractions <- vector(length=length(borders)-1)
+  for (i in seq(1, length(borders)-1)) {
+    subdata<-subset(data, bl>borders[i] & bl<borders[i+1])
+    # Size of sample in current baseline bin
+    size<-length(subdata$bl)
+    # Generate sample of sources
+    mu_logv0<-x[2]
+    std_logv0<-x[3]
+    mu_logtb<-x[4]
+    std_logtb<-x[5]
+    beta_e<-x[6]
+    logv0 = rnorm(size, mu_logv0, std_logv0)
+    logtb = rnorm(size, mu_logtb, std_logtb)
+    e = rbeta(size, 5, beta_e)
+    dfi = runif(size, 0, pi/2)
+    fluxes<-flux_ell(cbind(subdata$bl, exp(logv0), exp(logtb), e, dfi))
+    # Count detections
+    detections<-subset(subdata, fluxes>5.*subdata$s_thr)
+    fractions[i]<-as.double(length(detections$bl))/size
+  }
+  return(fractions)
+}
+
 # TODO: Implement model with each single source having the same parameters
 # even if it is observed on different baselines
 # function must accept vector of model parameters and return summary statistics
@@ -138,8 +164,8 @@ model3<-function(x){
 library("EasyABC")
 library("abc")
 # Define priors
-prior1=list(c("normal", -0.43, 0.15), c("lognormal", -0.1, 0.15), c("unif", 26, 32), c("unif", 0, 7.5))
-prior2=list(c("normal", -0.43, 0.15), c("lognormal", -0.1, 0.15), c("unif", 26, 32), c("unif", 0, 7.5), c("unif", 1, 30))
+prior1=list(c("normal", -0.43, 0.15), c("normal", 0.94, 0.15), c("unif", 26, 32), c("unif", 0, 7.5))
+prior2=list(c("normal", -0.43, 0.15), c("normal", 0.94, 0.15), c("unif", 26, 32), c("unif", 0, 7.5), c("unif", 1, 30))
 # Data summary statistics
 sum_stat_obs=c(0.68085106, 0.43043478, 0.20297030, 0.09770115, 0.02116402)
 set.seed(1)
@@ -159,9 +185,11 @@ hist(ABC_rej$param[, 5], plot="True", main="Histogram of e", xlab="e", ylab="P(e
 hist(abc_out$adj.values[, 5], plot="True", main="Histogram of e", xlab="e", ylab="P(e | data)", freq=FALSE)
 
 # Doing PMC sampling
-# This setup results in 400s of computations
-ABC_seq<-ABC_sequential(method="Drovandi", model=model2, prior=prior2, nb_simul=20,
-                        summary_stat_target=sum_stat_obs, tolerance_tab=0.02)
+# This setup (20&0.02) results in 400s of computations
+ABC_seq<-ABC_sequential(method="Drovandi", model=model2, prior=prior2, nb_simul=100,
+                        summary_stat_target=sum_stat_obs, tolerance_tab=0.01)
+ABC_seq<-ABC_sequential(method="Drovandi", model=model2m, prior=prior2, nb_simul=50,
+                        summary_stat_target=sum_stat_obs, tolerance_tab=0.05, n_cluster=4, use_seed=TRUE)
 abc_seq_out<-abc(sum_stat_obs, ABC_seq$param, ABC_seq$stats, tol=0.5,
                  method="loclinear")
 
@@ -174,5 +202,41 @@ mu_logv0 = -0.8
 std_logv0 = 0.3
 mu_logtb = 29.0
 std_logtb = 0.7
-beta = 10.
+alpha_e = 33
+beta_e = 50.
+
 # Create ``n_s`` sources from population
+n_s = length(unique(data$source))
+sources = levels(data$sources)
+set.seed(1)
+logv0 = rnorm(n_s, mu_logv0, std_logv0)
+logtb = rnorm(n_s, mu_logtb, std_logtb)
+e = rbeta(n_s, alpha_e, beta_e)
+
+# Create container for new data
+new_data<-data
+# Filling new_data with params
+new_data$angles <- vector(length=length(new_data$bl))
+new_data$logv0 <- vector(length=length(new_data$bl))
+new_data$logtb <- vector(length=length(new_data$bl))
+new_data$e <- vector(length=length(new_data$bl))
+s=0
+for (i in seq(along=sources)) {
+  indxs<-which(new_data$source==new_data$source[i])
+  s<-s+length(indxs)
+  
+  for (j in seq(along=indxs)) {
+    new_data$e[indxs[j]]<-e[i]
+    new_data$logv0[indxs[j]]<-logv0[i]
+    new_data$logtb[indxs[j]]<-logtb[i]
+    new_data$angles[indxs[j]]<-runif(1, 0, pi/2)
+  }
+}
+
+
+
+
+fluxes<-flux_ell(cbind(subdata$bl, exp(logv0), exp(logtb), e, dfi))
+# Count detections
+detections<-subset(subdata, fluxes>5.*subdata$s_thr)
+fractions[i]<-as.double(length(detections$bl))/size
